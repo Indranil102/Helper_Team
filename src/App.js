57398688c90d13c9f3c7c96b37edc161dc0f0c9f
Indranil102 <indranilsamanta2003@gmail.com>
2024-12-19 17:55:27 +0530

    navaerbar modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+
 import Navbar from './Component/Navbar';
 
 function App() {
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -3,7 +3,22 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div className='Navbar'>
-      let create navbar 
+      <div className="helper">
+        <h2 >
+            HelperTeam
+        </h2>
+        <h4 >
+            PYQ
+        </h4>
+        <h4 >
+            Notes
+        </h4>
+        <h4>
+            Tutorials
+        </h4>
+        <button className='signin'>Sign in</button>
+
+      </div>
     </div>
   )
 }
