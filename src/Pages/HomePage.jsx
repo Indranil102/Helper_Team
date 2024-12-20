b1ccc64a536c62d9ba46cdecd3ebbefb7bc3bc70
Indranil102 <indranilsamanta2003@gmail.com>
2024-12-20 16:18:26 +0530

    navbar

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../Component/Navbar/Navbar'
+import HeroSection from '../Component/HeroSection/HeroSection'
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
-      
+      <HeroSection />
     </div>
   )
 }
