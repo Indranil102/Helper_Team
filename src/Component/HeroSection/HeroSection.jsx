7dd797cadce33c8579f89eee811f25611bd8b2fc
Indranil102 <indranilsamanta2003@gmail.com>
2024-12-20 15:52:55 +0530

    html content of herosection added

--- src/Component/HeroSection/HeroSection.jsx
+++ src/Component/HeroSection/HeroSection.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 import './HeroSection.css'
 const HeroSection = () => {
   return (
-    <div>
+    <div className='HeroSection'>
+        <div className="content">
+            <div className="left-Content">
+                <h2>HelperTeam</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet amet inventore tempore quos corrupti, ratione odit veritatis similique iste odio fugit minus nihil, quas nulla, illum praesentium. Error, ullam perspiciatis!</p>
+                <div className="container">
+                    <div className="click">
+                        <img src="" alt="" />
+                        <p>Lecutres</p>
+                    </div>
+                    <div className="click">
+                        <img src="" alt="" />
+                        <p>Notes</p>
+                    </div>
+                    <div className="click">
+                        <img src="" alt="" />
+                        <p>PYQ</p>
+                    </div>
+                    <div className="click">
+                        <img src="" alt="" />
+                        <p>TUTORIALS</p>
+                    </div>
+                    
+                </div>
+            </div>
+            <div className="right">
+                <img src="" alt="" />
+            </div>
+
+        </div>
       
     </div>
   )
