858b2122b48d43a1c134c2ce52b3397786d8cb56
Divyanshu-2004 <divyanshuhprajapati9@gmail.com>
2024-12-21 21:55:28 +0530

    address and folders are updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import HomePage from './Pages/home/HomePage';
+import About from './Pages/about/About';
+import Pyq from './Pages/pyq/Pyq';
+import Notes from './Pages/notes/Notes';
+import Lactures from './Pages/lactures/Lactures';
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path='./Pages/home/HomePage' element={<HomePage/>} />
+        <Route path='./Pages/lactures/Lactures' element={<Lactures/>} />
+        <Route path='./Pages/notes/Notes' element={<Notes/>} />
+        <Route path='./Pages/pyq/Pyq' element={<Pyq/>} />
+        <Route path='./Pages/about/About' element={<About/>} />
+
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/Pages/lactures/Lactures.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Lactures = () => {
+  return (
+    <div>
+      anushka ke liye Lactures
+    </div>
+  )
+}
+
+export default Lactures
