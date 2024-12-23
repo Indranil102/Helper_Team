8a8716e1eeb077185c4f4b84f0630fdfe3282e7a
Divyanshu-2004 <divyanshuhprajapati9@gmail.com>
2024-12-24 00:34:53 +0530

    pyq.css : dropdown button is updated

--- src/Pages/pyq/Pyq.jsx
+++ src/Pages/pyq/Pyq.jsx
@@ -12,11 +12,11 @@ const Pyq = () => {
         <div className='second-two'><img src="" alt="ik" /> PREVIOUS YEAR QUESTION </div>
         <div class="dropdown">
         <button class="dropdown-button">Dropdown <img src="" alt="dropdown" /></button>
-    <div class="dropdown-content">
-      <a href="#link1">Link 1</a>
-      <a href="#link2">Link 2</a>
-      <a href="#link3">Link 3</a>
-    </div>
+        <div class="dropdown-content">
+          <a href="#link1">Link 1</a>
+          <a href="#link2">Link 2</a>
+          <a href="#link3">Link 3</a>
+        </div>
     </div>
     <div class="dropdown">
         <button class="dropdown-button">Dropdown <img src="" alt="dropdown" /></button>
