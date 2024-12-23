8cc6d8b34be0d710aac31e4a561f116a409398ea
Indranil102 <indranilsamanta2003@gmail.com>
2024-12-23 23:53:18 +0530

    PYQ upadated html and css

--- src/Pages/pyq/Pyq.jsx
+++ src/Pages/pyq/Pyq.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
+import './Pyq.css'
 const Pyq = () => {
   return (
     <div>
+      <div className="navbar">
+
+      </div>
+
+      <div className="second-part">
+        <div className='second-two'><img src="" alt="ik" /> PREVIOUS YEAR QUESTION </div>
+        <div class="dropdown">
+        <button class="dropdown-button">Dropdown <img src="" alt="dropdown" /></button>
+    <div class="dropdown-content">
+      <a href="#link1">Link 1</a>
+      <a href="#link2">Link 2</a>
+      <a href="#link3">Link 3</a>
+    </div>
+    </div>
+    <div class="dropdown">
+        <button class="dropdown-button">Dropdown <img src="" alt="dropdown" /></button>
+    <div class="dropdown-content">
+      <a href="#link1">Link 1</a>
+      <a href="#link2">Link 2</a>
+      <a href="#link3">Link 3</a>
+    </div>
+    </div>
+        </div>
       
     </div>
   )
