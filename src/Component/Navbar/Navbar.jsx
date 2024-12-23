5998e6e804d3d941ecd9c40935bf7d74629b92c8
Indranil102 <indranilsamanta2003@gmail.com>
2024-12-23 22:42:16 +0530

    navbar file changed

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -3,28 +3,7 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div className='Navbar'>
-      <div className="helper">
-        <h2 >
-            HelperTeam
-        </h2>
-        <h4 >
-           <button> Home </button>
-        </h4>
-        <h4 >
-          <button>  PYQ </button>
-        </h4>
-        <h4 >
-           <button> Notes</button>
-        </h4>
-        <h4>
-        <button>JUET Server</button>
-        </h4>
-        <h4>
-        <button>About</button>
-        </h4>
-        <button className='signin'>Sign in</button>
-
-      </div>
+     
     </div>
   )
 }
