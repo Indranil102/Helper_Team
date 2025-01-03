436273311f40b8c856de9e110503a5762c12d05e
Divyanshu-2004 <divyanshuhprajapati9@gmail.com>
2025-01-04 00:38:12 +0530

    css updated

--- src/Pages/home/HomePage.jsx
+++ src/Pages/home/HomePage.jsx
@@ -35,7 +35,7 @@ const HomePage = () => {
                         <p>PYQ</p>
                     </div>
                     <div className="click">
-                        <HiOutlineServer size={9000} />
+                        <HiOutlineServer size={35} />
                         <p>JUET Server</p>
                     </div>
                     
