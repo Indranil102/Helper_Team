50847f6f38486da23fddfc5a12e20c2c2e9b6e23
Divyanshu-2004 <divyanshuhprajapati9@gmail.com>
2025-01-03 23:08:30 +0530

    update some of the files

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,40 +1,59 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Navbar.css'
 const Navbar = () => {
   return (
     <div >
       <div className='Navbar'>
       <ul className="helper">
+        
         <li>
             <h2 >
                 HelperTeam
             </h2>
         </li>
+        <Link to='/'>
         <li>
             <h4 >
                 <button> Home </button>
             </h4>
         </li>
+        </Link>
+        <Link to='/Pyq'>
         <li>
             <h4 >
                 <button>  PYQ </button>
             </h4>
         </li>
+        </Link>
+        <Link to='/Lectures'>
+        <li>
+            <h4>
+                <button>Lectures</button>
+            </h4>
+        </li>
+        </Link>
+        <Link to='/Notes'>
         <li>
             <h4 >
                 <button> Notes</button>
             </h4>
         </li>
+        </Link>
+        <Link to='/Juetserver'>
         <li>
             <h4>
                 <button>JUET Server</button>
             </h4>
         </li>
+        </Link>
+        <Link to='/About'>
         <li>
             <h4>
                 <button>About</button>
             </h4>
         </li>
+        </Link>
         <li>
             <button className='signin'>Sign in</button>
         </li>
---
+++ src/Pages/Juetserver/Juetserver.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Juetserver = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Juetserver
