0d9c590f25dec524a8051fb8ed2d9ac44a6e7941
Indranil102 <indranilsamanta2003@gmail.com>
2025-01-04 18:08:20 +0530

    notes css added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Component/Navbar/Navbar';
 import Pyq from './Pages/pyq/Pyq';
 import Lectures from './Pages/lectures/Lectures';
 import SignUp from './Component/signup/SignUp';
+import Notes from './Pages/notes/Notes';
 
 
 function App() {
@@ -28,6 +29,8 @@ function App() {
       <Route path='/Lectures' element={<Lectures/>} />
 
       <Route path='/signup' element={<SignUp/>} />
+
+      <Route path='/notes' element={<Notes/>} />
 
 
      
--- src/Pages/notes/Notes.jsx
+++ src/Pages/notes/Notes.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { CiStickyNote } from "react-icons/ci";
+import './Notes.css'
+
+
 
 const Notes = () => {
   return (
     <div>
-      
-    </div>
+      <div className="second-part">
+              <div className='second-two'><CiStickyNote /> PREVIOUS YEAR QUESTION </div>
+
+              <div class="dropdown">
+              <button class="dropdown-button">Foundation<MdOutlineKeyboardArrowDown /></button>
+          <div class="dropdown-content">
+            <a href="#link1">Link 1</a>
+            <a href="#link2">Link 2</a>
+            <a href="#link3">Link 3</a>
+          </div>
+          </div>
+             
+          </div>
+          
+              </div>
+    
   )
 }
 
