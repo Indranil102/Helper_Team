8bffb335f3e4b2b472fe5deaf2b93dea23382774
Indranil102 <indranilsamanta2003@gmail.com>
2025-01-04 22:30:27 +0530

    notes complete

--- src/Pages/notes/Notes.jsx
+++ src/Pages/notes/Notes.jsx
@@ -3,9 +3,23 @@ import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { CiStickyNote } from "react-icons/ci";
 import './Notes.css'
 
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { LuCloudDownload } from "react-icons/lu";
+import { IoLinkOutline } from "react-icons/io5";
+
 
 
 const Notes = () => {
+
+  const fileUrl1 = "https://drive.google.com/file/d/1Ece0MVz3K6eEmoZm4fiNHLhSEQyA427o/view?usp=drive_link";
+const fileUrl2 = "https://drive.google.com/file/d/1Ece0MVz3K6eEmoZm4fiNHLhSEQyA427o/view?usp=drive_link";
+const fileUrl3 = "https://drive.google.com/file/d/1Ece0MVz3K6eEmoZm4fiNHLhSEQyA427o/view?usp=drive_link";
+const fileUrl4 = "https://drive.google.com/file/d/1Ece0MVz3K6eEmoZm4fiNHLhSEQyA427o/view?usp=drive_link";
+const fileUrl5 = "https://drive.google.com/file/d/1Ece0MVz3K6eEmoZm4fiNHLhSEQyA427o/view?usp=drive_link";
+
+const getShortenedUrl = (url) => {
+  return url.length > 30 ? `${url.substring(0, 30)}...` : url;
+};
   return (
     <div>
       <div className="second-part">
@@ -21,6 +35,85 @@ const Notes = () => {
           </div>
              
           </div>
+          <div className="main-part">
+                <div className="left-part">
+                  <div className='info'>CT <RiArrowDropDownLine /></div>
+                  <div className='info'>CT <RiArrowDropDownLine /></div>
+                  <div className='info'>CT <RiArrowDropDownLine /></div>
+                  <div className='info'>CT <RiArrowDropDownLine /></div>
+                  <div className='info'>CT <RiArrowDropDownLine /></div>
+                  <div className='info'>CT <RiArrowDropDownLine /></div>
+                </div>
+                <div className="file-table">
+                  <div className="file-table-header">
+                    <h3>Title</h3>
+                    <h3>Share By</h3>
+                  </div>
+                 
+                  <div className="file-row">
+                      <div className="file-title">
+                          <LuCloudDownload />
+                          <p>May22 AN</p>
+                      </div>
+                      <div className="file-url">
+                          <a href={fileUrl1} target="_blank">
+                              <p class="paper_link"><IoLinkOutline />  {getShortenedUrl(fileUrl1)}</p>
+                          </a>
+                      </div>
+                  </div>
+                  <div className="file-row">
+                      <div className="file-title">
+                          <LuCloudDownload />
+                          <p>May22 AN</p>
+                      </div>
+                      <div className="file-url">
+                          <a href={fileUrl2} target="_blank">
+                              
+                              <p class="paper_link"><IoLinkOutline />  {getShortenedUrl(fileUrl2)}</p>
+                          </a>
+                      </div>
+                   </div>
+                   <div className="file-row">
+                      <div className="file-title">
+                          <LuCloudDownload />
+                          <p>May22 AN</p>
+                      </div>
+                      <div className="file-url">
+                          <a href={fileUrl3} target="_blank">
+                              
+                              <p class="paper_link"><IoLinkOutline />  {getShortenedUrl(fileUrl3)}</p>
+                          </a>
+                      </div>
+                   </div>
+                   <div className="file-row">
+                      <div className="file-title">
+                          <LuCloudDownload />
+                          <p>May22 AN</p>
+                      </div>
+                      <div className="file-url">
+                          <a href={fileUrl4} target="_blank">
+                              
+                              <p class="paper_link"><IoLinkOutline />  {getShortenedUrl(fileUrl4)}</p>
+                          </a>
+                      </div>
+                   </div>
+                   <div className="file-row">
+                      <div className="file-title">
+                          <LuCloudDownload />
+                          <p>May22 AN</p>
+                      </div>
+                      <div className="file-url">
+                          <a href={fileUrl5} target="_blank">
+                              
+                              <p class="paper_link"><IoLinkOutline />  {getShortenedUrl(fileUrl5)}</p>
+                          </a>
+                      </div>
+                   </div>
+                  
+                  </div>
+              </div>
+
+
           
               </div>
     
