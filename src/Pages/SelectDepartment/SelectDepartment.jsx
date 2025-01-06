4a6ce9c2daba90916033eecbe4d7e96c37eb8608
Indranil102 <indranilsamanta2003@gmail.com>
2025-01-06 22:58:28 +0530

    select dpartment

---
+++ src/Pages/SelectDepartment/SelectDepartment.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './SelectDepartment.css'
+const SelectDepartment = () => {
+  return (
+    <div>
+      <div className="select-year-fullscreen">
+          <h1>Select a Year</h1>
+          <div className="container">
+            <button className="box">
+              1 Year
+            </button>
+            <button className="box"  >
+              2 Year
+            </button>
+            <button className="box" >
+              3 Year
+            </button>
+            <button className="box"  >
+              4 Year
+            </button>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default SelectDepartment
