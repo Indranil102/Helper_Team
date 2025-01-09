c7081668bac75bded53d93e4d86e250fe9db8300
Indranil102 <indranilsamanta2003@gmail.com>
2025-01-10 00:35:48 +0530

    update department

--- src/Pages/Juetserver/Juetserver.jsx
+++ src/Pages/Juetserver/Juetserver.jsx
@@ -3,12 +3,10 @@ import './Juetserver.css'
 import SelectDepartment from '../SelectDepartment/SelectDepartment'
 const Juetserver = () => {
   return (
-    <div>
-     <SelectDepartment/>
-     
-
-     
-    </div>
+    <div className="juetserver-container">
+    <h1>JUET Server</h1>
+    <SelectDepartment />
+  </div>
   )
 }
 
