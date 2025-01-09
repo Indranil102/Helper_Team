d15f22bce32b33a6568c5505ef179cfe382a987f
Divyanshu-2004 <divyanshuhprajapati9@gmail.com>
2025-01-10 00:26:21 +0530

    updated css path

--- src/Pages/Juetserver/Juetserver.jsx
+++ src/Pages/Juetserver/Juetserver.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import 'Juetserver.css'
+import {SelectDepartment} from '../SelectDepartment/SelectDepartment'
 const Juetserver = () => {
   return (
     <div>
      <SelectDepartment/>
-     
-
      
     </div>
   )
