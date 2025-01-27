8e9b104517811aa66d937dc29e4fcfa04f1e142a
Indranil102 <indranilsamanta2003@gmail.com>
2025-01-27 15:14:13 +0530

    update

--- src/Pages/about/About.jsx
+++ src/Pages/about/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './About.css'
+import indu from '../../Images/IMG_20240328_121030_301-removebg-preview.png'
 const About = () => {
   return (
     <>
@@ -8,9 +9,16 @@ const About = () => {
     </div>
     <div>
       <div className='container-about'>
-        <div><img src="" alt="" /></div>
-        <div><h5>Name</h5>
-             <p>Enroll</p></div>
+        <div ><img className='image'src={indu} alt="Indu" /></div>
+        <div className='personal-details'><h5>Indranil Samanta</h5>
+             <p>221B183</p>
+             <p>Prof.: Developer</p></div>
+      </div>
+      <div className='container-about'>
+        <div ><img className='image'src={''} alt="Pajju" /></div>
+        <div className='personal-details'><h5>Divyanshu Prajapati</h5>
+             <p>221B155</p>
+             <p>Prof.: Developer</p></div>
       </div>
     </div>
     </>
