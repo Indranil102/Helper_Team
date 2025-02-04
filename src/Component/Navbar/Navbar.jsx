90c04e7f607a5d4ef247f9b8b01e4f63f5e6b98c
Divyanshu-2004 <divyanshuhprajapati9@gmail.com>
2025-02-04 17:09:53 +0530

    textdecoration

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
     const [menuActive, setMenuActive] = useState(false);
-
+    const location = useLocation();
+    const [active, setActive] = useState(location.pathname);
+    const handleButtonClick = (path) => {
+        setActive(path); // Update the active state
+    };
     const toggleMenu = () => {
         setMenuActive(!menuActive);
     };
@@ -18,13 +22,14 @@ const Navbar = () => {
                 ☰
             </button>
             <ul className={`helper ${menuActive ? 'active' : ''}`}>
-                <Link to="/"><li><button>Home</button></li></Link>
-                <Link to="/Pyq"><li><button>PYQ</button></li></Link>
+                <Link to="/"><li><button className={active === '/' ? 'active' : ''} onClick={() => handleButtonClick('/')}>Home</button></li></Link>
+                <Link to="/Pyq"><li><button className={active === '/Pyq' ? 'active' : ''} onClick={() => handleButtonClick('/Pyq')}>PYQ</button></li></Link>
                 {/* <Link to="/Lectures">*/}<li><button onClick={notavailable}>Lectures</button></li>{/*</Link> */}
                 {/* <Link to="/Notes">*/}<li><button onClick={notavailable}>Notes</button></li>{/*</Link> */}
-                <Link to="/Juetserver"><li><button>JUET Server</button></li></Link>
-                <Link to="/About"><li><button>About</button></li></Link>
-                <Link to="/signup"><li><button className="signin">Sign in</button></li></Link>
+                <Link to="/Juetserver"><li><button className={active === '/Juetserver' ? 'active' : ''} onClick={() => handleButtonClick('/Juetserver')}>JUET Server</button></li></Link>
+                <Link to="/About"><li><button className={active === '/About' ? 'active' : ''} onClick={() => handleButtonClick('/About')}>About</button></li></Link>
+                <Link to="/signup"><li><button className={`signin ${active === '/signup' ? 'active' : ''}`}
+                            onClick={() => handleButtonClick('/signup')}>Sign in</button></li></Link>
             </ul>
         </div>
     );
