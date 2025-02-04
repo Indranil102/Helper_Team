294a35961017fae407ba8c2b41ff8483796cfb60
Indranil102 <indranilsamanta2003@gmail.com>
2025-02-04 16:37:37 +0530

    complete

--- src/Pages/notes/Notes.jsx
+++ src/Pages/notes/Notes.jsx
@@ -1,123 +1,15 @@
-// import React from 'react'
-// import { MdOutlineKeyboardArrowDown } from "react-icons/md";
-// import { CiStickyNote } from "react-icons/ci";
-// import './Notes.css'
-// import { RiArrowDropDownLine } from "react-icons/ri";
-// import { LuCloudDownload } from "react-icons/lu";
-// import { IoLinkOutline } from "react-icons/io5";
+
 
 import { FaLock } from "react-icons/fa6";
 const Notes = () => {
 
-// const fileUrl1 = "https://drive.google.com/file/d/1Ece0MVz3K6eEmoZm4fiNHLhSEQyA427o/view?usp=drive_link";
-// const fileUrl2 = "https://drive.google.com/file/d/1Ece0MVz3K6eEmoZm4fiNHLhSEQyA427o/view?usp=drive_link";
-// const fileUrl3 = "https://drive.google.com/file/d/1Ece0MVz3K6eEmoZm4fiNHLhSEQyA427o/view?usp=drive_link";
-// const fileUrl4 = "https://drive.google.com/file/d/1Ece0MVz3K6eEmoZm4fiNHLhSEQyA427o/view?usp=drive_link";
-// const fileUrl5 = "https://drive.google.com/file/d/1Ece0MVz3K6eEmoZm4fiNHLhSEQyA427o/view?usp=drive_link";
 
-// const getShortenedUrl = (url) => {
-//   return url.length > 30 ? `${url.substring(0, 30)}...` : url;
-// };
   return (
-//     <div>
-//       <div className="second-part-n">
-//               <div className='second-two-n'><CiStickyNote /> PREVIOUS YEAR QUESTION </div>
 
-//               <div class="dropdown-n">
-//               <button class="dropdown-button-n">CHOOSE YEAR<MdOutlineKeyboardArrowDown /></button>
-//           <div class="dropdown-content-n">
-//             <a href="#link1">I-YEAR</a>
-//             <a href="#link2">II-YEAR</a>
-//             <a href="#link3">III-YEAR</a>
-//             <a href="#link4">IV-YEAR</a>
-//           </div>
-//           </div>
            
-<div class="lock">
-<svg class="lock-icon"><FaLock /></svg>
+<div className="lock">
+<svg className="lock-icon"><FaLock size={100} /></svg>
 </div>
-//           </div>
-//           <div className="main-part-n">
-//                 <div className="left-part-n">
-//                   <div className='info-n'>CT <RiArrowDropDownLine /></div>
-//                   <div className='info-n'>CT <RiArrowDropDownLine /></div>
-//                   <div className='info-n'>CT <RiArrowDropDownLine /></div>
-//                   <div className='info-n'>CT <RiArrowDropDownLine /></div>
-//                   <div className='info-n'>CT <RiArrowDropDownLine /></div>
-//                   <div className='info-n'>CT <RiArrowDropDownLine /></div>
-//                 </div>
-//                 <div className="file-table-n">
-//                   <div className="file-table-header-n">
-//                     <h3>Title</h3>
-//                     <h3>Share By</h3>
-//                   </div>
-                 
-//                   <div className="file-row-n">
-//                       <div className="file-title-n">
-//                           <LuCloudDownload />
-//                           <p>May22 AN</p>
-//                       </div>
-//                       <div className="file-url-n">
-//                           <a href={fileUrl1} >
-//                               <p class="paper_link-n"><IoLinkOutline />  {getShortenedUrl(fileUrl1)}</p>
-//                           </a>
-//                       </div>
-//                   </div>
-//                   <div className="file-row-n">
-//                       <div className="file-title-n">
-//                           <LuCloudDownload />
-//                           <p>May22 AN</p>
-//                       </div>
-//                       <div className="file-url-n">
-//                           <a href={fileUrl2} >
-                              
-//                               <p class="paper_link-n"><IoLinkOutline />  {getShortenedUrl(fileUrl2)}</p>
-//                           </a>
-//                       </div>
-//                    </div>
-//                    <div className="file-row-n">
-//                       <div className="file-title-n">
-//                           <LuCloudDownload />
-//                           <p>May22 AN</p>
-//                       </div>
-//                       <div className="file-url-n">
-//                           <a href={fileUrl3} >
-                              
-//                               <p class="paper_link-n"><IoLinkOutline />  {getShortenedUrl(fileUrl3)}</p>
-//                           </a>
-//                       </div>
-//                    </div>
-//                    <div className="file-row-n">
-//                       <div className="file-title-n">
-//                           <LuCloudDownload />
-//                           <p>May22 AN</p>
-//                       </div>
-//                       <div className="file-url-n">
-//                           <a href={fileUrl4} >
-                              
-//                               <p class="paper_link-n"><IoLinkOutline />  {getShortenedUrl(fileUrl4)}</p>
-//                           </a>
-//                       </div>
-//                    </div>
-//                    <div className="file-row-n">
-//                       <div className="file-title-n">
-//                           <LuCloudDownload />
-//                           <p>May22 AN</p>
-//                       </div>
-//                       <div className="file-url-n">
-//                           <a href={fileUrl5} >
-                              
-//                               <p class="paper_link-n"><IoLinkOutline />  {getShortenedUrl(fileUrl5)}</p>
-//                           </a>
-//                       </div>
-//                    </div>
-                  
-//                   </div>
-//               </div>
-
-
-          
-//               </div>
     
   )
 }
